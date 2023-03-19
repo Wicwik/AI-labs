380827f142c77d3267bf0b9047c4e05794030f49
Robert Belanec <belanecrobert22@gmail.com>
2023-03-19 16:34:28 +0100

    uloha2

--- 05_cvicenie/uloha2/players.py
+++ 05_cvicenie/uloha2/players.py
@@ -1,6 +1,7 @@
 import random
 from games import TicTacToe, Gomoku
-import math
+import numpy as np
+import copy
 
 ################################### PLAYERS ###################################
 
@@ -25,36 +26,98 @@ class RandomPlayer(Player):
         # Picks random move from list of possible ones.
         return random.choice(game.actions(state))
 
+class Node:
+    '''
+    Structure to simplify the tree search
+    '''
+
+    def __init__(self, state, parent=None) -> None:
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.n = 0
+        self.value = 0
+
+    def __str__(self) -> str:
+        return f'state:{self.state} visited: {self.n}, value: {self.value}'
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
+    def update(self, result):
+        self.n += 1
+        self.value += result
+
+    def uct(self):
+        # Constatnt c is 2 as recommended by slides
+        return self.value / self.n + 2*np.sqrt(np.log(self.parent.n) / self.n) if self.n > 0 else np.inf
+
+def mcts(game, state, max_iter=10):
+    # This is the root node from which we are searching
+    root = Node(state)
+
+    for _ in range(max_iter):
+        # In each iteration we start from the root node
+        current_node = root
+
+        # Selection
+        # We want to expand a node with the highies uct, so we iterate over the nodes highiest uct until we found one that is not expanded 
+        while not game.is_terminal(current_node.state) and current_node.children:
+            current_node = max(current_node.children, key=lambda child: child.uct())
+
+        # Expansion
+        # Now we want to expand the selected node by generating its neighboring states from available actions
+        # After that, we chose randomly (uniformly) from the generated child nodes
+        if not game.is_terminal(current_node.state):
+            actions = game.actions(current_node.state)
+
+            for action in actions:
+                child_state = game.state_after_move(current_node.state, action)
+                child_node = Node(child_state, parent=current_node)
+                current_node.add_child(child_node)
+
+                # If we found terminal state child we can go right to the backpropagation
+                if game.is_terminal(child_state):
+                    break
+
+            current_node = random.choice(current_node.children)
+
+        # Simulation / Rollout
+        # Now we want to randomly descend to the leaf of the tree
+        current_state = current_node.state
+        while not game.is_terminal(current_state):
+            current_state = game.state_after_move(current_state, random.choice(game.actions(current_state)))
+
+        # Backpropagation
+        # When we are at the leaf of the tree we can propagate up to the root and update each node along the way value based on the result
+        my_player = game.player_at_turn(state)
+        result = game.utility(current_state, my_player)
+        while current_node:
+            current_node.update(result)
+            current_node = current_node.parent
+            result = result
+
+    # When we are done searching we just return the child of root that was visited the most, which means that under this node is the winning state with higiest probablity
+    return max(root.children, key=lambda child: child.n).state
+
+
 
 class MCTSearchPlayer(Player):
     def choose_move(self, game, state):
-        # FIXME implement this method to return the best move according to Monte Carlo Tree Search algorithm
-        ### Examples:
-        # my_player = game.player_at_turn(state)
-        # opponent = game.other_player(my_player)
-        # board = game.board_in_state(state)
-        # if board[0][1] == my_player: ...
-        # possible_actions = game.actions(state)
-        # some_action = possible_actions[0]
-        # new_state = game.state_after_move(current_state, some_action)
-        # if game.is_terminal(some_state): ...
-        # utility = game.utility(some_state, my_player)2
+        '''
+        We start from the current state and compute mcts each time before we take any action
+        The more iterations we take, the bigger the probability of choosing the optimal move
+        100 iters had over 90% win rate against random player
+        '''
+        probably_next_best_state = mcts(game, state, max_iter=100)
+        probably_best_action = None
 
-        c = 2
+        # once we got the state, we just need to find the right action
+        for action in game.actions(state):
+            if game.state_after_move(state, action) == probably_next_best_state:
+                probably_best_action = action
 
-        # Selection
-
-        # Expansion
-
-        # Simulation
-
-        # backpropagation
-
-        print('actions:', game.actions(state), 'state:', state)
-        my_player = game.player_at_turn(state)
-        print(game.utility(state, my_player))
-
-        return game.actions(state)[0]  # dummy return
+        return probably_best_action
 
 
 
@@ -68,7 +131,7 @@ if __name__ == '__main__':
 
     # TicTacToe().play([RandomPlayer(), AskingPlayer()], show_moves=show_moves)
     # TicTacToe().play([MCTSearchPlayer(), AskingPlayer()], show_moves=show_moves)
-    TicTacToe().play([MCTSearchPlayer(), RandomPlayer()], show_moves=show_moves)
+    # TicTacToe().play([MCTSearchPlayer(), RandomPlayer()], show_moves=show_moves)
 
-    # TicTacToe().play_n_games([MCTSearchPlayer(), RandomPlayer()], n=100)
-    # TicTacToe().play_n_games([MCTSearchPlayer(), MCTSearchPlayer()], n=100)
+    TicTacToe().play_n_games([MCTSearchPlayer(), RandomPlayer()], n=100)
+    TicTacToe().play_n_games([MCTSearchPlayer(), MCTSearchPlayer()], n=100)
